3dc89ccbb31224675d8f206c57189e8dfa479e97
Rosenfeld <rosenfeldge@nih.gov>
2024-03-05 10:01:35 -0500

    Update to latest bioconductor version

--- installation.R
+++ installation.R
@@ -2,11 +2,13 @@
 install.packages("tidyverse")
 
 
-# Bioconductor packages (running with R 4.3 and version 3.17 of Bioconductor)
+# Bioconductor packages (running with R 4.3 and version 3.18 of Bioconductor)
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-BiocManager::install("ggcyto", version = "3.17")
-BiocManager::install("flowCore", version = "3.17")
-BiocManager::install("flowAI", version = "3.17")
-BiocManager::install("openCyto", version = "3.17")
+BiocManager::install(version = "3.18")
+
+BiocManager::install("ggcyto", version = "3.18")
+BiocManager::install("flowCore", version = "3.18")
+BiocManager::install("flowAI", version = "3.18")
+BiocManager::install("openCyto", version = "3.18")
